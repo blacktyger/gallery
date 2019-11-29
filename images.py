48f6b0bf787e73612c334ea1696cddcd2fb83878
blacktyger <blacktyg3r@gmail.com>
2019-11-29 14:02:33 +0000

    maping images in all dirs and subdirs

--- images.py
+++ images.py
@@ -6,55 +6,55 @@ import shutil
 import pickle
 
 
+location = 'E:\gallery\django_project\\blog\static\Zdjecia\Szkocja'
+
+
 # files formats
 def img_format():
-    return ('.JPG', '.jpg', '.jpeg', '.JPEG', '.BMP', '.bmp', '.PNG', '.png')
-
-
-def vid_format():
-    pass
+    return '.JPG', '.jpg', '.jpeg', '.JPEG', '.BMP', '.bmp', '.PNG', '.png'
 
 
 # all files paths
-def paths(folder_name='2016', main_path='E:\gallery\django_project\\blog\static\Zdjecia'):
-    ctx = {
-        'main_path': main_path,
-        'folder_name': folder_name,
-        'folder_path': main_path + '\\' + folder_name,
-    }
-    return ctx
+def file_path():
+    files_path = []
+    # r=>root, d=>directories, f=>files
+    for r, d, f in os.walk(location):
+        for item in f:
+            if item.endswith(img_format()):
+                files_path.append(os.path.join(r, item))
+    return files_path
 
 
 # get file info from title (date, time)
 def file_info(file):
+    title = str(file.split('\\')[-1])
     ctx = {
-        'full_path': f"{paths()['folder_path']}\\{file}",
-        'year': str(file.split('_')[1][:4]),
-        'month': str(file.split('_')[1][4:6]),
-        'day': str(file.split('_')[1][-2:]),
-        'time': f"{str(file.split('_')[2][:2])}:{str(file.split('_')[2][2:4])}",
+        'path': file,
+        'year': str(title.split('_')[1][:4]),
+        'month': str(title.split('_')[1][4:6]),
+        'day': str(title.split('_')[1][-2:]),
+        'time': f"{str(title.split('_')[2][:2])}:{str(title.split('_')[2][2:4])}",
     }
     return ctx
 
 
 # make list of all files in folder
-img_files = [img for img in os.listdir(paths()['folder_path']) if img.endswith(img_format())]
-print(f"All files in {paths()['folder_name']}: {len(os.listdir(paths()['folder_path']))}")
+img_files = [img for img in file_path() if img.endswith(img_format())]
 print(f"Images: {len(img_files)}")
-
-files_info = paths()['folder_name'] + '.txt'
 
 
 # save everything to JSON file
 def files_to_json():
-    with open(files_info, 'w') as f:
-        photos = [file_info(img) for img in img_files if len(img) > 20]
-        nonames = [img for img in img_files if len(img) < 15]
+    with open('all_files.txt', 'w') as f:
+        photos = [file_info(img) for img in img_files if len(img.split('\\')[-1]) > 20]
+        nonames = [img for img in img_files if len(img.split('\\')[-1]) < 15]
         json.dump({'photos': photos, 'nonames': nonames}, f, indent=4)
+
+files_to_json()
 
 
 # access files details from JSON file
-with open(files_info) as f:
+with open('all_files.txt') as f:
     link_list = json.load(f)
     print(link_list['photos'][1])
 
@@ -65,7 +65,7 @@ def test_random():
     x = link_list['photos'][random.randint(10, 50)]
     print(x['month'], x['day'], x['time'])
 
-    img = Image.open(x['full_path'])
+    img = Image.open(x['path'])
     img.show()
 
 
