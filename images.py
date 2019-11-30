c1a727f5bd77c983c7af13ee1f82855cad779eb7
blacktyger <blacktyg3r@gmail.com>
2019-11-30 21:41:56 +0000

    New way to extraxt data from _ALL_ files (os.stats()), display method

--- images.py
+++ images.py
@@ -1,12 +1,11 @@
-import os
+import os, time
 import json
 import random
 from PIL import Image
 import shutil
 import pickle
-
-
-location = 'E:\gallery\django_project\\blog\static\Zdjecia\Szkocja'
+from datetime import datetime
+from stat import *
 
 
 # files formats
@@ -16,6 +15,7 @@ def img_format():
 
 # all files paths
 def file_path():
+    location = 'E:\gallery\django_project\\blog\static\Zdjecia'
     files_path = []
     # r=>root, d=>directories, f=>files
     for r, d, f in os.walk(location):
@@ -25,48 +25,48 @@ def file_path():
     return files_path
 
 
-# get file info from title (date, time)
+# get file info (date, time)
 def file_info(file):
-    title = str(file.split('\\')[-1])
+    stats = datetime.fromtimestamp(os.stat(file)[ST_MTIME])
     ctx = {
         'path': file,
-        'year': str(title.split('_')[1][:4]),
-        'month': str(title.split('_')[1][4:6]),
-        'day': str(title.split('_')[1][-2:]),
-        'time': f"{str(title.split('_')[2][:2])}:{str(title.split('_')[2][2:4])}",
+        'year': stats.strftime("%Y"),
+        'month': [stats.strftime("%B"), stats.strftime("%m")],
+        'day': [stats.strftime("%A"), stats.strftime("%d")],
+        'time': stats.strftime("%H:%M"),
     }
     return ctx
 
 
-# make list of all files in folder
-img_files = [img for img in file_path() if img.endswith(img_format())]
-print(f"Images: {len(img_files)}")
-
-
 # save everything to JSON file
 def files_to_json():
+    img_files = [img for img in file_path() if img.endswith(img_format())]
+    print(f"Images: {len(img_files)}")
     with open('all_files.txt', 'w') as f:
-        photos = [file_info(img) for img in img_files if len(img.split('\\')[-1]) > 20]
-        nonames = [img for img in img_files if len(img.split('\\')[-1]) < 15]
-        json.dump({'photos': photos, 'nonames': nonames}, f, indent=4)
-
-files_to_json()
+        photos = [file_info(img) for img in img_files]
+        json.dump({'photos': photos}, f, indent=4)
 
 
-# access files details from JSON file
-with open('all_files.txt') as f:
-    link_list = json.load(f)
-    print(link_list['photos'][1])
+def display_info(file):
+    dir = f"Info: " + file['path'].split('\\')[-2]
+    date = f"Photo Date: {file['day'][0]}, {file['day'][1]} {file['month'][0]} {file['year']}"
+    time = f"Photo Time: {file['time']}"
+    return f"{dir} \n{date} \n{time} \n"
 
 
-# test
+# TESTING AREA ---------------------------------------file------------------------------------------------
 def test_random():
-    files_to_json()  # map folder
-    x = link_list['photos'][random.randint(10, 50)]
-    print(x['month'], x['day'], x['time'])
+    with open('all_files.txt') as f:
+        link_list = json.load(f)
+        counter = 0
+        while counter < 5:
+            x = link_list['photos'][random.randint(0, 13946)]
+            if x['year'] == '2018':
+                print(display_info(x))
+                # img = Image.open(x['path'])
+                # img.show()
+                counter += 1
 
-    img = Image.open(x['path'])
-    img.show()
 
-
+#files_to_json()
 test_random()
